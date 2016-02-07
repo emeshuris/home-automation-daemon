f037c061c10c2778b6a61047ee3059cb03b6cf29
Edward M. Meshuris <edward@theemm.com>
2016-02-07 11:13:36 -0800

    .

--- gpio.py
+++ gpio.py
@@ -1,9 +1,5 @@
 from gpio_common import GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-
-
 def up():
     chan_list = [10]
     GPIO.setup(chan_list, GPIO.OUT)
--- gpio_common.py
+++ gpio_common.py
@@ -2,4 +2,4 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-chan_list = [10]
+chan_list = [8,10]
